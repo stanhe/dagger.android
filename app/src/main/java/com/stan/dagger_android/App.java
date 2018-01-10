a0f42d36676409ce66a4598b0743b127b126257a
uu-PC\uu <hshl4314@gmail.com>
2018-01-10 17:29:49 +0800

    Add logger

--- app/src/main/java/com/stan/dagger_android/App.java
+++ app/src/main/java/com/stan/dagger_android/App.java
@@ -1,5 +1,8 @@
 package com.stan.dagger_android;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
 import com.stan.dagger_android.di.DaggerAppComponent;
 
 import dagger.android.AndroidInjector;
@@ -15,6 +18,18 @@ public class App extends DaggerApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        initLogger();
+    }
+
+    private void initLogger() {
+        Logger.addLogAdapter(new AndroidLogAdapter(
+                PrettyFormatStrategy.newBuilder()
+                        .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+                        .methodCount(5)         // (Optional) How many method line to show. Default 2
+                        .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+                        .tag("Stan")            // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                        .build()
+        ));
     }
 
     @Override
