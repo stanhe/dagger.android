2e6c7bf6cd00d3d3fc0019529d6c7588c82d7754
uu-PC\uu <hshl4314@gmail.com>
2018-04-03 09:21:37 +0800

    Update dependencies
    update gradle dep version.
    change scop annotation from java to kotlin.

--- app/src/main/java/com/stan/dagger_android/di/ActivityScoped.kt
+++ app/src/main/java/com/stan/dagger_android/di/ActivityScoped.kt
@@ -1,9 +1,5 @@
 package com.stan.dagger_android.di
 
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
 import javax.inject.Scope
 
 /**
@@ -12,7 +8,7 @@ import javax.inject.Scope
  * scope to be used by all fragment components. Additionally, a component with a specific scope
  * cannot have a sub component with the same scope.
  */
-@Documented
+@MustBeDocumented
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScoped
--- app/src/main/java/com/stan/dagger_android/di/FragmentScoped.kt
+++ app/src/main/java/com/stan/dagger_android/di/FragmentScoped.kt
@@ -1,11 +1,9 @@
 package com.stan.dagger_android.di
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 import javax.inject.Scope
 
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class FragmentScoped
